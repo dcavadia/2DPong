e98fa952562ac0a5cfa3a83eaffd11051fd0a4d6
dcavadia <53955221+dcavadia@users.noreply.github.com>
2022-03-26 23:39:26 -0400

    add menu manager
    add main menu

---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : SingletonComponent<MenuManager>
+{
+    public override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(this);
+    }
+
+    public void GoToMenu(MenuName name)
+    {
+        switch (name)
+        {
+            case MenuName.Help:
+                SceneManager.LoadScene("HelpMenu");
+                break;
+            case MenuName.Main:
+
+                SceneManager.LoadScene("MainMenu");
+                break;
+            case MenuName.Pause:
+                Object.Instantiate(Resources.Load("PauseMenu"));
+                break;
+        }
+    }
+
+    public void HandlePlayButtonOnClickEvent()
+    {
+        SceneManager.LoadScene("2DPong");
+    }
+
+    public void HandleHelpButtonOnClickEvent()
+    {
+        GoToMenu(MenuName.Help);
+    }
+
+    public void HandleQuitButtonOnClickEvent()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/UI/PlayButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayButton : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        MenuManager.Instance.HandlePlayButtonOnClickEvent();
+    }
+}
