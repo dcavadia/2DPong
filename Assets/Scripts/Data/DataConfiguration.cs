9b39e599357c3beb688bcfea7b01d69469fc753e
dcavadia <53955221+dcavadia@users.noreply.github.com>
2022-03-26 22:00:18 -0400

    update data configuration values for game

--- Assets/Scripts/Data/DataConfiguration.cs
+++ Assets/Scripts/Data/DataConfiguration.cs
@@ -12,6 +12,17 @@ public class DataConfiguration
     //default
     float paddleMoveUnitsPerSecond = 10;
     float ballImpulseForce = 200;
+    float ballLifeSeconds = 10;
+    float minSpawnSeconds = 5;
+    float maxSpawnSeconds = 10;
+    int standardBlockPoints = 1;
+    int bonusBlockPoints = 2;
+    int pickupBlockPoints = 5;
+    float standardBlockProbability = 0.7f;
+    float bonusBlockProbability = 0.2f;
+    float freezerBlockProbability = 0.05f;
+    float speedupBlockProbability = 0.05f;
+    int ballsPerGame = 5;
 
 
     public DataConfiguration()
@@ -49,11 +60,77 @@ public class DataConfiguration
         get { return ballImpulseForce; }
     }
 
+    public float BallLifeSeconds
+    {
+        get { return ballLifeSeconds; }
+    }
+
+    public float MinSpawnSeconds
+    {
+        get { return minSpawnSeconds; }
+    }
+
+    public float MaxSpawnSeconds
+    {
+        get { return maxSpawnSeconds; }
+    }
+
+    public int StandardBlockPoints
+    {
+        get { return standardBlockPoints; }
+    }
+
+    public int BonusBlockPoints
+    {
+        get { return bonusBlockPoints; }
+    }
+
+    public int PickupBlockPoints
+    {
+        get { return pickupBlockPoints; }
+    }
+
+    public float StandardBlockProbability
+    {
+        get { return standardBlockProbability; }
+    }
+
+    public float BonusBlockProbability
+    {
+        get { return bonusBlockProbability; }
+    }
+
+    public float FreezerBlockProbability
+    {
+        get { return freezerBlockProbability; }
+    }
+
+    public float SpeedupBlockProbability
+    {
+        get { return speedupBlockProbability; }
+    }
+
+    public int BallsPerGame
+    {
+        get { return ballsPerGame; }
+    }
+
     void SetConfigurationDataFields(string csvValues)
     {
         string[] values = csvValues.Split(',');
         paddleMoveUnitsPerSecond = float.Parse(values[0]);
         ballImpulseForce = float.Parse(values[1]);
+        ballLifeSeconds = float.Parse(values[2]);
+        minSpawnSeconds = float.Parse(values[3]);
+        maxSpawnSeconds = float.Parse(values[4]);
+        standardBlockPoints = int.Parse(values[5]);
+        bonusBlockPoints = int.Parse(values[6]);
+        pickupBlockPoints = int.Parse(values[7]);
+        standardBlockProbability = float.Parse(values[8]) / 100;
+        bonusBlockProbability = float.Parse(values[9]) / 100;
+        freezerBlockProbability = float.Parse(values[10]) / 100;
+        speedupBlockProbability = float.Parse(values[11]) / 100;
+        ballsPerGame = int.Parse(values[12]);
     }
 
 }
--- Assets/Scripts/Data/GameConfiguration.cs
+++ Assets/Scripts/Data/GameConfiguration.cs
@@ -21,4 +21,59 @@ public static class GameConfiguration
         get { return dataConfiguration.BallImpulseForce; }
     }
 
+    public static float BallLifeSeconds
+    {
+        get { return dataConfiguration.BallLifeSeconds; }
+    }
+        
+    public static float MinSpawnSeconds
+    {
+        get { return dataConfiguration.MinSpawnSeconds; }    
+    }
+
+    public static float MaxSpawnSeconds
+    {
+        get { return dataConfiguration.MaxSpawnSeconds; }    
+    }
+
+    public static int StandardBlockPoints
+    {
+        get { return dataConfiguration.StandardBlockPoints; }    
+    }
+        
+    public static int BonusBlockPoints
+    {
+        get { return dataConfiguration.BonusBlockPoints; }    
+    }
+
+    public static int PickupBlockPoints
+    {
+        get { return dataConfiguration.PickupBlockPoints; }    
+    }
+
+    public static float StandardBlockProbability
+    {
+        get { return dataConfiguration.StandardBlockProbability; }    
+    }
+
+    public static float BonusBlockProbability
+    {
+        get { return dataConfiguration.BonusBlockProbability; }    
+    }
+
+    public static float FreezerBlockProbability
+    {
+        get { return dataConfiguration.FreezerBlockProbability; }    
+    }
+
+    public static float SpeedupBlockProbability
+    {
+        get { return dataConfiguration.SpeedupBlockProbability; }    
+    }
+
+    public static int BallsPerGame
+    {
+        get { return dataConfiguration.BallsPerGame; }    
+    }
+
 }
